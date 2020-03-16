72f12092541e3208f54d6eebf3ba495c20a88fb9
okfriansyah <okfriansyah@doku.com>
2020-03-17 00:01:34 +0700

    get, put, delete, update rest controller users

--- src/main/java/com/itrainmyself/springboot/controller/UserController.java
+++ src/main/java/com/itrainmyself/springboot/controller/UserController.java
@@ -3,10 +3,16 @@ package com.itrainmyself.springboot.controller;
 import com.itrainmyself.springboot.entity.User;
 import com.itrainmyself.springboot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -21,5 +27,30 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getAllUSers() {
         return userService.getAllUsers();
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<User> getUserById(@PathVariable("id") Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/users/{id}")
+    public User updateUserById(@PathVariable("id") Long id, @RequestBody User user) {
+        return userService.updateUserById(id, user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUserById(@PathVariable("id") Long id) {
+        userService.deleteUserById(id);
+    }
+
+    @GetMapping("/users/byusername/{username}")
+    public User getUserByUserName(@PathVariable("username") String username) {
+        return userService.getUserByUsername(username);
     }
 }
--- src/main/java/com/itrainmyself/springboot/services/UserService.java
+++ src/main/java/com/itrainmyself/springboot/services/UserService.java
@@ -2,16 +2,15 @@ package com.itrainmyself.springboot.services;
 
 import com.itrainmyself.springboot.entity.User;
 import com.itrainmyself.springboot.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private UserRepository userRepository;
 
-    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -19,4 +18,29 @@ public class UserService {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user;
+    }
+
+    public User updateUserById(Long id, User user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    public void deleteUserById(Long id) {
+        if (userRepository.findById(id).isPresent()) {
+            userRepository.deleteById(id);
+        }
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
 }
